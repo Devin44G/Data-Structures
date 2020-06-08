25d216761855532b82728beb48c47d2195c0b3e6
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-06-08 16:28:43 -0500

    Push method works, will work on pop later - Moving on to queues

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,4 @@
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order.
@@ -28,20 +29,74 @@ return elements in Last In First Out order.
 #             self.size = len(self.storage)
 #             return value
 
-# class Stack:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = []
-#
-#     def __len__(self):
-#         return self.size
-#
-#     def push(self, value):
-#         self.storage.append(value)
-#         self.size = len(self.storage)
-#
-#     def pop(self):
-#         if self.storage != []:
-#             value = self.storage.pop()
-#             self.size = len(self.storage)
-#             return value
+
+# from guided project
+class Node:
+    def __init__(self, value=None, next_node=None):
+        # value at this node
+        self.value = value
+        # reference to the next node in the list of nodes
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        # set this node's next reference to the passed in node
+        self.next_node = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        # first node in the list
+        self.head = None
+        # last node in the list
+        self.tail = None
+
+    # we don't have access to the end of the linked list
+    # when we want to add to the end, we need to traverse the whole linked list to get to the end
+    # O(n) - linear
+
+    # we have direct access to end of the list so we can add nodes to it directly
+    def add_to_tail(self, value):
+        # regardless of if the list is empty or not, we need to wrap the value in a Node
+        new_node = Node(value)
+        # what if the list is empty?
+        if not self.head and not self.tail:
+            # set both head and tail to new_node
+            self.head = new_node
+            self.tail = new_node
+        # what if the list isn't empty?
+        else:
+            # set the current tail's next to the new node
+            self.tail.set_next(new_node)
+            # set self.tail to the new node
+            self.tail = new_node
+
+    def remove_tail(self):
+        if self.head is None:
+            return None
+        else:
+            self.head = self.head.get_value()
+            return self.head
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size = self.size + 1
+
+    def pop(self):
+        if self.size > 0:
+            self.size = self.size - 1
+            self.storage.remove_tail()
